d467c587a1ed8afe2cd079eacc5331891d5bcc0a
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-08-29 17:40:07 +0300

    finished Task 1

---
+++ js/task-01.js
@@ -0,0 +1,24 @@
+const listEl = document.querySelector('#categories');
+console.log(listEl);
+// console.log(listEl.children);
+
+
+const itemsRef = document.querySelectorAll('.item');
+// console.log(itemsRef);
+console.log(`В списке ${itemsRef.length} категории.`);
+
+
+const titleEl = document.querySelectorAll("h2");
+// console.log(`Категория:`, titleEl[0].textContent);
+// console.log(`Количество элементов:`, titleEl[0].nextElementSibling.children.length);
+
+// console.log(`Категория:`, titleEl[1].textContent);
+// console.log(`Количество элементов:`, titleEl[2].nextElementSibling.children.length);
+
+// console.log(`Категория:`, titleEl[2].textContent);
+// console.log(`Количество элементов:`, titleEl[2].nextElementSibling.children.length);
+const itemsContent = titleEl.forEach(elem => console.log(
+    `Категория: ${elem.textContent}
+Количество елементов: ${elem.nextElementSibling.children.length}`
+));
+
