1d8ebdeb785dbcc331d5a2a0ac5f7c78cda1a3cc
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-09-11 02:40:47 +0300

    added new object

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,64 @@
+
+
+
 const controlRefs = {
     input: document.querySelector('input'),
     render: document.querySelector('button[data-action="render"]'),
     destroy: document.querySelector('button[data-action="destroy"]'),
+    boxes: document.querySelector('#boxes'),
 };
-const { input, render, destroy } = controlRefs;
+const { input, render, destroy, boxes } = controlRefs;
 
-const createBoxesHandler = amount => {
+boxes.style.paddingTop = 10;
+
+
+
+const createBoxesHandler = (amount) => {
     amount = Number(input.value);
-    console.log(amount);
+    console.log(`input.value:`, amount);
 
     let boxSize = 30;
+    const elements = [];
+    for (let i = 1; i <= amount; i += 1) {
+        console.log(amount.length);
+        console.log(i); // = (input.value === кол-во элементов)
+        // console.log(amount === i);
+        boxSize += 10;
 
-    boxes.innerHTML = [...Array(amount)]
-        .map(() => {
-            boxSize += 10;
-            return `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color: rgb(${colorHandler()});" ></div>`;
-        })
-        .join('')
+        const boxEl = document.createElement('div');
+        boxEl.style.width = `${boxSize}px`;
+        boxEl.style.height = `${boxSize}px`;
+        boxEl.style.backgroundColor = `rgb(${colorHandler()})`;
+
+        boxEl.style.border = ('2px ridge blue');
+        boxEl.style.marginTop = ('5px');
+            
+        elements.push(boxEl);
+
+    };
+    // if (elements.length === amount) {
+        
+    // }
+    // console.log(elements);
+    return boxes.append(...elements);
+    
 };
+
+
+// const createBoxesHandler = (amount) => {
+//     amount = Number(input.value);
+//     console.log(amount);
+//     let boxSize = 30;
+//     boxes.innerHTML = [...Array(amount)]
+//         .map(() => {
+//             boxSize += 10;
+//             return `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color: rgb(${colorHandler()});" ></div>`;
+//         })
+//         .join('')
+// };
+
+
+
 
 const destroyBoxesHandler = () => {
     boxes.innerHTML = '';
@@ -32,8 +73,8 @@ const colorHandler = () => {
     return [r, g, b];
 };
 
-render.addEventListener("click", createBoxesHandler);
-destroy.addEventListener("click", destroyBoxesHandler);
+render.addEventListener('click', createBoxesHandler);
+destroy.addEventListener('click', destroyBoxesHandler);
 
 
 
